3196c8a3eff7d36b2b3c5ddabb3906e7bade4fee
Nicolas Galvão <nicolas.9299@colegioprogresso.g12.br>
2026-06-12 16:26:02 -0300

    Atualizar o forum.js

--- forum.js
+++ forum.js
@@ -1,21 +1,121 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs
+}
+from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASgoTtFKGHAOf9jMRgDk9w0jYvXeslEnw",
-  authDomain: "forum-domoustache.firebaseapp.com",
-  projectId: "forum-domoustache",
-  storageBucket: "forum-domoustache.firebasestorage.app",
-  messagingSenderId: "609863036952",
-  appId: "1:609863036952:web:0bd971da4f4db16bf7d36d",
-  measurementId: "G-ESREQB30TB"
+
+    apiKey: "AIzaSyASgoTtFKGHAOf9jMRgDk9w0jYvXeslEnw",
+
+    authDomain: "forum-domoustache.firebaseapp.com",
+
+    projectId: "forum-domoustache",
+
+    storageBucket: "forum-domoustache.firebasestorage.app",
+
+    messagingSenderId: "609863036952",
+
+    appId: "1:609863036952:web:0bd971da4f4db16bf7d36d",
+
+    measurementId: "G-ESREQB30TB"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
+
+const publicar =
+document.getElementById("publicar");
+
+publicar.addEventListener("click", async () => {
+
+    const nome =
+    document.getElementById("nome").value;
+
+    const comentario =
+    document.getElementById("comentario").value;
+
+    const nota =
+    parseInt(
+        document.getElementById("nota").value
+    );
+
+    if(!nome || !comentario){
+
+        alert(
+            "Preencha todos os campos."
+        );
+
+        return;
+    }
+
+    await addDoc(
+        collection(db, "avaliacoes"),
+        {
+            nome,
+            comentario,
+            nota,
+            likes: 0
+        }
+    );
+
+    location.reload();
+
+});
+
+async function carregarAvaliacoes(){
+
+    const lista =
+    document.getElementById(
+        "lista-avaliacoes"
+    );
+
+    lista.innerHTML = "";
+
+    const docs =
+    await getDocs(
+        collection(db, "avaliacoes")
+    );
+
+    docs.forEach((doc) => {
+
+        const dados =
+        doc.data();
+
+        lista.innerHTML += `
+
+            <div class="avaliacao">
+
+                <h3>
+                    ${dados.nome}
+                </h3>
+
+                <p>
+                    ${"⭐".repeat(dados.nota)}
+                </p>
+
+                <p>
+                    ${dados.comentario}
+                </p>
+
+                <button class="likes-btn">
+
+                    👍 ${dados.likes}
+
+                </button>
+
+            </div>
+
+        `;
+
+    });
+
+}
+
+carregarAvaliacoes();
